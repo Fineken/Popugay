7c9e2705a82dbae5f16a46eb918150798d905f64
Fineken <danilzolotarev1980@gmail.com>
2023-12-12 18:40:30 +0300

    2

--- Popugay/Popygai.py
+++ Popugay/Popygai.py
@@ -1,117 +1,31 @@
-from flask import Flask, request, render_template_string
+import time
+import psutil
+import os
 
-app = Flask(__name__)
+# Директория для хранения логов
+logs_dir = 'logs'
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
 
-HTML_TEMPLATE = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>ПО-ПУ-ГАЙ</title>
-    <style>
-        body {
-            background-color: orange;
-            text-align: center;
-            font-family: Arial, sans-serif;
-            color: black;
-        }
-        h1 {
-            color: black;
-        }
-        form {
-            margin-top: 20px;
-        }
-        input[type="text"] {
-            margin-top: 10px;
-        }
-        input[type="submit"] {
-            margin-top: 10px;
-        }
-        .parrot {
-            font-family: "Courier New", Courier, monospace;
-            text-align: left;
-        }
-        ul {
-            text-align: left;
-            display: inline-block;
-            margin: 0;
-            padding: 0;
-        }
-        li {
-            list-style: none;
-        }
-    </style>
-</head>
-<body>
-    <div class="parrot">
-        <pre>
-________________________________________$$$$$$$$$___$$$
-____________________________________$$$$$$$$_____(&)_$$$$
-________________________________$$$$$$$$$$____________$$$$
-_____________________________$$$$$$$$$$$$$$$____§§§§_____$
-___________________________$$$$$$$$$$$$$$$$$$§§§§_§§§_____$
-_________________________$$$$$$$$$$$$$$$$$$$$$§§§§§_§____$
-______________________$$$$$$$$$$$$$$$$$$_$$$$___§§§__§_$
-____________________$$$$$$$$$$$$$_$$$$$$$$_$________§
-___________________$$$$$$$$$$$$$$$$$$$$$$$$_$
-_________________$$$$$$$$$_$$$$$$$$$$$$$$$$_$
-_______________$$$$$$$$_§http://risuns.ru$$$$$_$
-_____________$$$$$$$_$$§§§$$$§$$§§$$$$§§$_$$$
-____________$$$$$$_§§§$$§§$$§§§$$§§$$§§$_$$$
-___________$$$$$_$§§§$$$$§§§$$$§§§§§$$_$$$$$
-__________$$$$$_§§§§§§$$§§§$$§§§§§§§_$$$$$$
-_________$$$$_§§0§§§§§§§§§§§§§§§§_$$$$$$$
-________$$$_000§§§00§§§§§000§00§_$$$$$$
-________$$_0000§§0000§§§00§0§_$$$$$$
-_______$_0000§§00000§0000_$$$$$$$
-_____§§00000§000§0000_$$$$$$$$
-___§00000§0000000_$$$$$$$$$$
-___000§00000§_§§§§$$$$$$$$$$$$$___......
-__0000§00§§§§§§$$$$$$$$__$$$$$$$$§§§§§
-_000§0_§§§§§§$$$$$$$_§000_$$$§§§_0000000000000000000
-00____§§§§§$$$$$$$_§§00000_$§§§_00000000000000000000
-______§§§§$$$$$$$_§§§_’’______§§§__’’
-_____§§§$$$$$$
-____§§$$$$$$$
-___$$$$$$$$
-__$$$$$$$$
-_$$$$$$$$
-$$$$$$$$
-$$$$$$$
-$$$$$$
-$$$$
-$$$
-        </pre>
-    </div>
-    <h1>5 КУРС, ПОПУГАЙ--ПРОЕКТ </h1>
-    <form method="POST">
-        <input type="text" name="text_input" placeholder="Введите текст"/>
-        <input type="submit" value="Отправить"/>
-        <h1></h1>
-        <h1></h1>
-        <h1></h1>
-        <h1></h1>
-    </form>
-    {% if text_output %}
-        <ul>
-        {% for word in text_output %}
-            <li>{{ word }}</li>
-        {% endfor %}
-        </ul>
-    {% endif %}
-</body>
-</html>
-'''
+# Определение пути к файлу для записи логов
+log_filename = os.path.join(logs_dir, f'system_load_{int(time.time())}.txt')
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    text_output = []
-    if 'text_output' in request.form:
-        text_output = request.form['text_output'].split(',')
-    if request.method == 'POST':
-        text_input = request.form['text_input']
-        if text_input:
-            text_output.append(text_input + ' ' + text_input)
-    return render_template_string(HTML_TEMPLATE, text_output=text_output)
+# Мониторим систему в течение 10 секунд
+monitoring_duration = 10  # секунды
+end_time = time.time() + monitoring_duration
 
-if __name__ == '__main__':
-    app.run(debug=True)
+while time.time() < end_time:
+    # Получаем данные о загрузке системы
+    cpu_percent = psutil.cpu_percent()
+    memory_percent = psutil.virtual_memory().percent
+
+    # Записываем данные в файл логов
+    with open(log_filename, 'a') as log_file:
+        log_file.write(f'Timestamp: {time.ctime()}\n')
+        log_file.write(f'CPU Load: {cpu_percent}%\n')
+        log_file.write(f'Memory Usage: {memory_percent}%\n')
+        log_file.write('\n')  # Разделитель между записями
+
+    time.sleep(1)  # Пауза в 1 секунду между измерениями
+
+print(f'Мониторинг завершен. Результаты сохранены в файл: {log_filename}')
